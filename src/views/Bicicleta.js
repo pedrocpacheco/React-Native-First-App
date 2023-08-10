13e6aba9d8edc21f9ad8e1036eb5fd5fb83ce179
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-10 10:53:38 -0300

    style: titulo style at Bicicleta

--- src/views/Bicicleta.js
+++ src/views/Bicicleta.js
@@ -8,7 +8,7 @@ const width = Dimensions.get("screen").width;
 export default function Bicileta(){
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text>Detalhes da Bicicleta</Text>
+        <Text style={estilos.titulo}>Detalhes da Bicicleta</Text>
     </>
 }
 
@@ -16,5 +16,18 @@ const estilos = StyleSheet.create({
     topo: {
         width: "100%",
         height: 578 / 768 * width - 10,
+    },
+    titulo:{
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",    
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 10,
+        textShadowColor: "black",
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius: 6
     }
 })
