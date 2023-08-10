de8f38ec3d292850a42e9097c918e0fbcb820ba1
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-10 12:11:42 -0300

    feat: owner view with owner image"

--- src/views/Bicicleta.js
+++ src/views/Bicicleta.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from "react-native"; 
 
 import top from "../../assets/bicicles/top.png"
+import owner_image from "../../assets/owner.png"
 
 const width = Dimensions.get("screen").width;
 
@@ -9,12 +10,15 @@ export default function Bicileta(){
     return <>
         <Image source={top} style={styles.top} />
         <Text style={styles.title}>Detalhes da Bicicleta</Text>
-        <View>
+        <View style={styles.infos}>
             <Text style={styles.name}>Bicicleta Allied</Text>
-            <Text style={styles.owner_name}>Pedro Carvalho Pacheco</Text>
+            <View>
+                <Image source={owner_image}></Image>
+                <Text style={styles.owner_name}>Pedro Carvalho Pacheco</Text>
+            </View>
             <Text style={styles.description}>
                 Uma bicicleta esportiva com alta 
-                performance para atletas profissionais
+                performance para atletas profissionais.
             </Text>
             <Text style={styles.price}>R$5.000,00</Text>
         </View>
@@ -40,7 +44,7 @@ const styles = StyleSheet.create({
         textShadowRadius: 6
     },
     infos: {
-        paddingVertical: 8,
+        paddingVertical: 10,
         paddingHorizontal: 16
     },
     name: {
@@ -49,20 +53,22 @@ const styles = StyleSheet.create({
         lineHeight: 42,
         fontWeight: "bold"
     },
-   owner_name:{
-    fontSize: 16,
-    lineHeight: 26
+    owner_name:{
+        fontSize: 18,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontWeight: "bold"
    },
    description: {
-    color: "#A3A3A3",
-    fontSize: 16,
-    lineHeight: 26
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
    },
    price: {
-    color: "#2A9F85",
-    fontWeight: "bold",
-    fontSize: 26,
-    lineHeight: 42,
-    marginTop: 6
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 6
    }
 });
