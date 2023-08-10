fb7bbbfce8b2694067cd85420718789d0a7a3103
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-10 10:54:50 -0300

    refactor: change variables language to english

--- src/views/Bicicleta.js
+++ src/views/Bicicleta.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text } from "react-native"; 
 
-import topo from "../../assets/topo.png"
+import top from "../../assets/top.png"
 
 const width = Dimensions.get("screen").width;
 
 export default function Bicileta(){
     return <>
-        <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhes da Bicicleta</Text>
+        <Image source={top} style={styles.top} />
+        <Text style={styles.title}>Detalhes da Bicicleta</Text>
     </>
 }
 
-const estilos = StyleSheet.create({
-    topo: {
+const styles = StyleSheet.create({
+    top: {
         width: "100%",
         height: 578 / 768 * width - 10,
     },
-    titulo:{
+    title:{
         width: "100%",
         position: "absolute",
         textAlign: "center",    
